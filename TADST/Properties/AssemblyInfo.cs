baa30fb5e3159e19655598a856e51929b7c26384
Christoffer Wadensten <cwadensten@gmail.com>
2017-09-23 07:32:20 +0200

    updated assembly info

--- TADST/Properties/AssemblyInfo.cs
+++ TADST/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("jymden.com")]
 [assembly: AssemblyProduct("TADST")]
-[assembly: AssemblyCopyright("© 2016 by Christoffer Wadensten")]
+[assembly: AssemblyCopyright("© 2017 by Christoffer Wadensten")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
